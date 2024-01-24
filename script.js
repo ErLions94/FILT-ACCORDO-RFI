6c78dc60f5a17097926958543cc8f806d11c1c35
ErLions94 <114865892+ErLions94@users.noreply.github.com>
2024-01-25 00:18:35 +0100

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function cambiaTesto() {
-  var testo = document.getElementById("testoCambiante");
+function toggleText() {
+    var smallScreenText = document.querySelector('.small-screen-text');
+    var largeScreenText = document.querySelector('.large-screen-text');
 
-  if (testo.innerHTML === "Testo iniziale") {
-    testo.innerHTML = "Nuovo testo!";
-  } else {
-    testo.innerHTML = "Testo iniziale";
-  }
+    if (smallScreenText.style.display === 'block') {
+        smallScreenText.style.display = 'none';
+        largeScreenText.style.display = 'block';
+    } else {
+        smallScreenText.style.display = 'block';
+        largeScreenText.style.display = 'none';
+    }
 }
