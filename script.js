d078e96431334dfbb0ce08feecb053c9d3960c07
ErLions94 <114865892+ErLions94@users.noreply.github.com>
2024-01-25 00:09:28 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-.
+function cambiaTesto() {
+  var testo = document.getElementById("testoCambiante");
+
+  if (testo.innerHTML === "Testo iniziale") {
+    testo.innerHTML = "Nuovo testo!";
+  } else {
+    testo.innerHTML = "Testo iniziale";
+  }
+}
